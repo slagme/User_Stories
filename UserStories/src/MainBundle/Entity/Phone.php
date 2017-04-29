9bf4635b66481063816364edcb8d25a4897eff2e
slagme <slawomir.gmerek@gmail.com>
2017-04-29 22:15:56 +0200

    Entity Phone Made

---
+++ UserStories/src/MainBundle/Entity/Phone.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Phone
+ *
+ * @ORM\Table(name="phone")
+ * @ORM\Entity(repositoryClass="MainBundle\Repository\PhoneRepository")
+ */
+class Phone
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="PhoneNumber", type="string", length=20)
+     */
+    private $phoneNumber;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Type", type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="MainBundle\Entity\Contact", inversedBy="phones")
+     * @ORM\JoinColumn(name="contact_id", referencedColumnName="id")
+     */
+    private $contact;
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set phoneNumber
+     *
+     * @param string $phoneNumber
+     * @return Phone
+     */
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get phoneNumber
+     *
+     * @return string 
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Phone
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set contact
+     *
+     * @param \MainBundle\Entity\Phone $phone
+     * @return Address
+     */
+    public function setPhone(\MainBundle\Entity\Phone $phone = null)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+    /**
+     * Get contact
+     *
+     * @return \MainBundle\Entity\Phone
+     */
+    public function getPhone()
+    {
+        return $phone->phone;
+    }
+}
